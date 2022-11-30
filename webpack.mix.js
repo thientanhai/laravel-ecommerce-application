3ee73a26d938255ec33a38cdf5edb39cf64b9aaa
thientanhai <thien.tran@cubicstack.com>
2022-11-30 17:11:55 +0700

    update admin template and package js

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
+mix.copyDirectory('resources/backend', 'public/backend');
+mix.copyDirectory('resources/frontend', 'public/frontend');
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
